1f2cc73582e114c61bbd4a002a1434971159c4f2
art238 <art23831@gmail.com>
2025-10-23 14:46:52 +0300

    KS-2-1 create task

---
+++ src/main/kotlin/Lesson_2/les2_task1.kt
@@ -0,0 +1,13 @@
+package Lesson_2
+
+fun main() {
+
+    val people1: Float = 3f
+    val people2: Float = 4f
+    val people3: Float = 3f
+    val people4: Float = 5f
+
+    val averageRating = (people1 + people2 + people3 + people4) / 4
+    println(averageRating)
+
+}
