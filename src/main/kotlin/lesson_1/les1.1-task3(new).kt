9bdd2626e0d4937ad45e0ab54e0a8a0180963507
art238 <art23831@gmail.com>
2025-10-24 16:32:49 +0300

    KS-1.1-3-(new) refactor the task

---
+++ src/main/kotlin/lesson_1/les1.1-task3(new).kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+
+    val year = 1961
+    var hour = 9
+    var minutes = 7
+
+    println(year)
+    println(hour)
+    println(minutes)
+    println()
+
+    hour = 10
+    minutes = 55
+
+    println("$hour:$minutes")
+}
