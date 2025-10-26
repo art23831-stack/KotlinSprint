699fd1ca5498e80c6cef250875b911890c1bd271
art238 <art23831@gmail.com>
2025-10-26 14:13:04 +0300

    KS-4-2 completed the task.

--- src/main/kotlin/Lesson_4/les4_task2.kt
+++ src/main/kotlin/Lesson_4/les4_task2.kt
@@ -8,7 +8,7 @@ fun main() {
     val weight2 = 50
     val volume2 = 100
 
-    println("Груз с весом 20 кг и объемом 80 л соответствует категории 'Average':${weight1 > 35 && weight1 <= 100 && volume1 < 100}")
-    println("Груз с весом 50 кг и объемом 100 л соответствует категории 'Average':${weight2 > 35 && weight2 <= 100 && volume2 < 100}")
+    println("Груз с весом 20 кг и объемом 80 л соответствует категории \"Average\":${weight1 > 35 && weight1 <= 100 && volume1 < 100}")
+    println("Груз с весом 50 кг и объемом 100 л соответствует категории \"Average\":${weight2 > 35 && weight2 <= 100 && volume2 < 100}")
 
 }
