e57a192a26ec5507e41c2f4a9b363ae094cb8589
art238 <art23831@gmail.com>
2025-10-27 00:28:24 +0300

    KS-2-2 completed the task.

---
+++ src/main/kotlin/Lesson_2/less2_task2.kt
@@ -0,0 +1,20 @@
+package org.example.Lesson_2
+
+fun main() {
+
+    val workers = 50
+    val trainees = 20
+    val salaryWorkers = 30000
+    val salaryTrainees = 20000
+
+    val paymentsSalaryWorkers = workers * salaryWorkers
+    val paymentsSalaryTrainees = trainees * salaryTrainees
+    val allPayments = paymentsSalaryTrainees + paymentsSalaryWorkers
+    val averageSalary = allPayments / (workers + trainees)
+
+    println(paymentsSalaryWorkers)
+    println(paymentsSalaryTrainees)
+    println(allPayments)
+    println(averageSalary)
+
+}
