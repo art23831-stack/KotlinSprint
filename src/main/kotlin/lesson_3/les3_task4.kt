6c84b3d92cb41cee8fa09c626685db5f463f01a1
art238 <art23831@gmail.com>
2025-10-26 10:56:06 +0300

    KS-3-4 completed the task.

---
+++ src/main/kotlin/lesson_3/les3_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var numberMove = 1
+
+    println( "$from-$to;$numberMove")
+
+    from = "D2"
+    to = "D3"
+    numberMove += 1
+
+    println("$from-$to;$numberMove")
+
+}
