6349167412596b03fb35804c514d59278d20dc96
art238 <art23831@gmail.com>
2025-10-26 23:15:12 +0300

    KS-4-4 corrected the task.

---
+++ src/main/kotlin/Lesson_4/les4_task5.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson_4
+
+fun main() {
+
+    val damage: Boolean = false
+    val crew: Int = 60
+    val provision: Int = 55
+    val weatherGood: Boolean = true
+    val weatherBad: Boolean = true
+
+    val itIsPossibleToSend: Boolean =
+        (!damage && crew >= 55 && crew <=70
+            && provision > 50 && weatherGood || weatherBad)
+    || (crew == 70 && weatherGood && provision > 50 )
+
+    println(itIsPossibleToSend)
+
+}
