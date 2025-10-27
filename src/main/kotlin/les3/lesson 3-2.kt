3c0b6a0eaa6eaafc652da4aa5d4a8fdbe7a6e5a7
art238 <art23831@gmail.com>
2025-10-27 23:05:34 +0300

    KS-3-2 corrected the task.

--- src/main/kotlin/les3/lesson 3-2.kt
+++ src/main/kotlin/les3/lesson 3-2.kt
@@ -3,13 +3,15 @@ package org.example.les3
 fun main() {
 
     val name = "Tatyana"
-    val surname1 = "Andreeva"
+    var surname = "Andreeva"
     val patronymic = "Sergeevna"
-    val surname2 = "Sidorova"
     var age = 20
 
-    println("$surname1 $name $patronymic:$age")
+    println("$surname $name $patronymic:$age")
+
     age = 22
-    println("$surname2 $name $patronymic:$age")
+    surname = "Sidorova"
+
+    println("$surname $name $patronymic:$age")
 
 }
