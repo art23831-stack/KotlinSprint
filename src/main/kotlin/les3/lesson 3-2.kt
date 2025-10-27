3692fd6e79f7b1a8bd12ec4fc8d302a35c0fb33a
art238 <art23831@gmail.com>
2025-10-27 16:47:51 +0300

    KS-3-2 completed the task.

---
+++ src/main/kotlin/les3/lesson 3-2.kt
@@ -0,0 +1,17 @@
+package org.example.les3
+
+fun main() {
+
+    val name = "Tatyana"
+    val surname1 = "Andreeva"
+    val patronymic = "Sergeevna"
+    val surname2 = "Sidorova"
+    val age1 = 20
+    val age2 = 22
+
+    val nspAge1 = "$surname1 $name $patronymic:$age1"
+    val nspAge2 = "$surname2 $name $patronymic:$age2"
+
+    println(nspAge1)
+    println(nspAge2)
+}
