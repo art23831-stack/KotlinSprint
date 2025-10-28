5cd6c34837d21627756140913acd25150665e0cf
art238 <art23831@gmail.com>
2025-10-28 21:23:51 +0300

    KS-2-5 corrected the task.

--- src/main/kotlin/Lesson_2/les2_task5.kt
+++ src/main/kotlin/Lesson_2/les2_task5.kt
@@ -5,10 +5,11 @@ import kotlin.math.pow
 fun main() {
 
     val sum = 70000
-    val interestRate = 0.167
+    val interestRate = 16.7
+    val interestRatePrecentFormat = interestRate / 100
     val years = 20
 
-    val totalAmount = sum * (1 + interestRate).pow(years)
+    val totalAmount = sum * (1 + interestRatePrecentFormat).pow(years)
     val formattedTotalAmount = "%.3f".format(totalAmount)
     println(formattedTotalAmount)
 }
